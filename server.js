5e7727461f9be40bc2dfe9ef23518623307729ff
Nikyshah07 <nikyshah07@gmail.com>
2025-04-21 00:33:27 +0530

    new change

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const app = express();
 const registerRoute=require('./routes/Register.js')
 const loginRoute=require('./routes/Login.js')
 const signinGoogle=require('./routes/SignWithGoogle.js')
-
+const cors=require('cors');
+app.use(cors())
 // Middleware
 app.use(express.json({ limit: '50mb' })); // Increased limit for base64 images
 
@@ -24,6 +25,10 @@ initializeDatabase();
 app.use('/',registerRoute);
 app.use('/',loginRoute)
 app.use('/',signinGoogle)
+
+app.get('/',(req,res)=>{
+    res.send('hello from backend')
+})
 
 // Start server
 const PORT = process.env.PORT || 5000;
